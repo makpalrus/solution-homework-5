a0b76b0ba32dab7dcf6a6cc60127f35df3309af8
Makpal <maqpal.rus@gmail.com>
2025-04-05 10:02:22 +0500

    Add MarkerFactory class for Flyweight pattern

--- flyweight/MarkerFactory.java
+++ flyweight/MarkerFactory.java
@@ -1,4 +1,22 @@
 package flyweight;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MarkerFactory {
+    public static final Map<String, Marker> markers = new HashMap<>();
+
+    public static Marker getMarker(String type) {
+        if (!markers.containsKey(type)) {
+            markers.put(type, new ConcreteMarker(type));
+        }
+        return markers.get(type);
+    }
+    public static void clearUnusedMarkers() {
+        markers.clear();
+    }
+
+    public static int getTotalMarkerTypesCreated() {
+        return markers.size();
+    }
+
 }
