368274de8c924e36a3c978f852554f01d412f922
Makpal <maqpal.rus@gmail.com>
2025-04-05 09:44:09 +0500

    Add again ProtectionProxyImage class for Proxy pattern

--- proxy/ProtectionProxyImage.java
+++ proxy/ProtectionProxyImage.java
@@ -1,4 +1,30 @@
 package proxy;
 
-public class ProtectionProxyImage {
+public class ProtectionProxyImage implements Image {
+    private String filename;
+    private RealImage realImage;
+    private boolean isAuthorized;
+
+    public ProtectionProxyImage(String filename, boolean isAuthorized) {
+        this.filename = filename;
+        this.isAuthorized = isAuthorized;
+    }
+
+    @Override
+    public void displayThumbnail() {
+        System.out.println("Displaying low-res thumbnail: " + filename);
+    }
+
+    @Override
+    public void displayFullImage() {
+        if (!isAuthorized) {
+            System.out.println("Access Denied: You must be logged in to view the full image.");
+            return;
+        }
+        if (realImage == null) {
+            realImage = new RealImage(filename);
+        }
+        realImage.displayFullImage();
+    }
 }
+
