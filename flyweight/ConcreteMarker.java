de0acd715610e6bf548bbc71f3f12e37881c7162
Makpal <maqpal.rus@gmail.com>
2025-04-05 09:55:45 +0500

    Add ConcreteMarker class for Flyweight pattern

--- flyweight/ConcreteMarker.java
+++ flyweight/ConcreteMarker.java
@@ -1,4 +1,15 @@
 package flyweight;
 
-public class ConcreteMarker {
+public class ConcreteMarker implements Marker {
+    private final String type;
+
+    public ConcreteMarker(String type) {
+        this.type = type;
+        System.out.println("Creating marker of type: " + type);
+    }
+
+    @Override
+    public void display(int x, int y) {
+        System.out.println("Displaying " + type + " at (" + x + ", " + y + ")");
+    }
 }
