eb9db88e721b4f160eebf6886de31e3a1a785022
Makpal <maqpal.rus@gmail.com>
2025-04-05 05:43:26 +0500

    update main

--- Main.java
+++ Main.java
@@ -1,13 +1,23 @@
 import proxy.Image;
+import proxy.ProtectionProxyImage;
 import proxy.ProxyImage;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("--- Proxy Pattern ---");
+        System.out.println(" Proxy Pattern ");
         Image image = new ProxyImage("house.jpg");
         image.displayThumbnail();
         image.displayFullImage();
+
+        System.out.println("\n Protection Proxy ");
+        Image protectedImage = new ProtectionProxyImage("home.jpg", false);
+        protectedImage.displayThumbnail();
+        protectedImage.displayFullImage();
+
+        Image authorizedImage = new ProtectionProxyImage("home.jpg", true);
+        authorizedImage.displayThumbnail();
+        authorizedImage.displayFullImage();
 
     }
 }
